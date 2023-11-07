5b37a464e3a6556ec7628c201cd14a7b93bb66f1
joonaro <cupetus@gmail.com>
2023-11-07 19:16:11 +0200

    cleaned code

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,18 +1,14 @@
 const toTop = document.querySelector('#backToTop');
-window.onscroll = () => {showToTop()};
+window.onscroll = () => { showToTop() };
 function showToTop() {
-    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-      toTop.style.display = "block";
-    } else {
-      toTop.style.display = "none";
-    }
+  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+    toTop.style.display = "block";
+  } else {
+    toTop.style.display = "none";
   }
-const toTopFunc = () => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
 }
-
-
-
-
+const toTopFunc = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 toTop.addEventListener('click', toTopFunc)
